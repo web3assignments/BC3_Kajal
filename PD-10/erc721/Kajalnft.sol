7c2bffb997863ee4899ef4845ccb6093ac71c01c
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-02-04 21:09:15 +0100

    Create Kajalnft.sol

---
+++ PD-10/erc721/Kajalnft.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract Kajalnft is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Kajalnft", "SNFT") public{
+    }
+
+    function CreateKajalnft(address tokenholder, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(tokenholder, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
+
