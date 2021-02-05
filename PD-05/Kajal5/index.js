7420544c404f07f6244d40aa146809e82cfec5a4
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-02-05 23:07:03 +0100

    Add files via upload

---
+++ PD-05/Kajal5/index.js
@@ -0,0 +1,185 @@
+//contract voor de deployment op rinkeby via remix
+
+const contract_address = '0x38B93bBF2c75599C81552E1C946AbfE95c00c17d';
+
+const abi = [
+
+{
+
+"inputs": [
+
+{
+
+"internalType": "uint256",
+
+"name": "_numbers",
+
+"type": "uint256"
+
+}
+
+],
+
+"name": "setNumbers",
+
+"outputs": [],
+
+"stateMutability": "nonpayable",
+
+"type": "function"
+
+},
+
+{
+
+"inputs": [],
+
+"name": "getNumbers",
+
+"outputs": [
+
+{
+
+"internalType": "uint256",
+
+"name": "",
+
+"type": "uint256"
+
+}
+
+],
+
+"stateMutability": "view",
+
+"type": "function"
+
+}
+
+]
+
+
+
+window.addEventListener('load', asyncloaded); 
+
+// Function from web3examples. responsible for loading the provider.
+
+async function asyncloaded() {
+
+web3 = new Web3(Web3.givenProvider); // provider from metamask 
+
+var result = await web3.eth.requestAccounts().catch(x => console.log(x.message));
+
+console.log(`web3 is present: ${web3.version}`); // note: use ` (back quote)
+
+const network = await web3.eth.net.getId().catch((reason) => console.log(`Cannnot find network ${reason}`));
+
+if (typeof network === 'undefined' || network != 4) { console.log("Please select Rinkeby test network"); return; }
+
+console.log("Ethereum network: Rinkeby")
+
+accounts = await web3.eth.getAccounts();
+
+console.log(accounts[0]); // show current user.
+
+contract = new web3.eth.Contract(abi, contract_address);
+
+}
+
+window.addEventListener('load', asyncloaded); 
+
+
+
+//functie op data te verzenden
+
+function storeNumbers(){
+
+var numberstored = document.getElementById("numberstored").value;
+
+console.log(numberstored);
+
+contract.methods.setNumbers(numberstored).send({from: accounts[0]});
+
+console.log(numberstored);
+
+}
+
+
+
+//functie om data op te halen
+
+function getNumbers() {
+
+//contract call naar de sol contract
+
+contract.methods.getNumbers().call().then( function( Numberss ) {
+
+console.log("this is the stored number: ", Numberss);
+
+document.getElementById('Numberss').innerHTML = Numberss;
+
+});
+
+}
+
+
+
+//voor de navigatie
+
+$(document).ready(function () {
+
+
+
+$('.navbar .dropdown-item').on('click', function (e) {
+
+var $el = $(this).children('.dropdown-toggle');
+
+var $parent = $el.offsetParent(".dropdown-menu");
+
+$(this).parent("li").toggleClass('open');
+
+
+
+if (!$parent.parent().hasClass('navbar-nav')) {
+
+if ($parent.hasClass('show')) {
+
+$parent.removeClass('show');
+
+$el.next().removeClass('show');
+
+$el.next().css({"top": -999, "left": -999});
+
+} else {
+
+$parent.parent().find('.show').removeClass('show');
+
+$parent.addClass('show');
+
+$el.next().addClass('show');
+
+$el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+
+}
+
+e.preventDefault();
+
+e.stopPropagation();
+
+}
+
+});
+
+
+
+$('.navbar .dropdown').on('hidden.bs.dropdown', function () {
+
+$(this).find('li.dropdown').removeClass('show open');
+
+$(this).find('ul.dropdown-menu').removeClass('show open');
+
+});
+
+
+
+});
