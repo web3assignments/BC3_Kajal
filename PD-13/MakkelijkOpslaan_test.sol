28469f0792ba48a6de3c773ae73262dd71bebb2f
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-02-16 19:58:42 +0100

    Update MakkelijkOpslaan_test.sol

--- PD-13/MakkelijkOpslaan_test.sol
+++ PD-13/MakkelijkOpslaan_test.sol
@@ -1,38 +1,37 @@
-pragma solidity >=0.4.22 <0.8.0;
-import "remix_tests.sol"; // this import is automatically injected by Remix.
-import "remix_accounts.sol";
-import "./MakkelijkOpslaan.sol";
+pragma solidity ^0.5.1;
+import "remix_tests.sol";
+import "./SimpleStorage.sol";
 
-// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
-contract testSuite {
+contract SimpleStorageTest {
+    
+    SimpleStorage private testContract;
+    
+    function beforeEach() external {
+        testContract = new SimpleStorage("Welkom");
+    }
+    
+    function shouldHaveAValue() external {
+        Assert.equal(testContract.value(), "Welkom", "Initial value should be 'Welkom' ");
+    }
 
-MakkelijkOpslaan save;
-    /// 'beforeAll' runs before all other tests
-    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
-    function beforeAll() public {
-       save = new MakkelijkOpslaan();
+    function shouldChangeValue() external {
+        testContract.setValue("Hoe gaat het?");
+        Assert.equal(testContract.value(), "Hoe gaat het?", "Value shoud have changed to 'Hoe gaat het?' ");
     }
-    //unit test 1 test the default values of number
-    function checkDefaultValueNumber() public returns (bool){
-        return Assert.equal(save.GetNumberData(), 5000, "The default value should be 5000");
-    }
-    // unit test 2 test of de default value 1000 is
-    function defaultValueShouldNotBeNumbers() public returns (bool){
-        return Assert.notEqual(save.GetNumberData(), 1000, "The default value only can be 5000");
-    }
-    //unit test 3 checks if the data is set to the new data
-    function checkNewNumbers() public returns (bool){
-      save.SaveNumberData(110);
-      return Assert.equal(save.GetNumberData(), 110, "value should be 110");
-  }
-    //unit test 4 test the default value of textdata
-    function checkDefaultValueTextData() public returns (bool){
-         return Assert.equal(save.GetTextData(), "testdata", "The default value should be 5000");
-    }
-  
-    //unit test 5check if the textdata is set to the new textdata
-    function checkNewTextData() public returns (bool){
-        save.saveTextData("text");
-        return Assert.equal(save.GetTextData(), "text", "The default testdata should be testdata");
-    }
+    	
+    function shoulddifferentiateValue() external {
+
+		testContract.setValue("Hoe gaat het?");
+		Assert.equal(testContract.value(), "Hoe gaat het?", "Value shoud have changed to 'Hoe gaat het?' ");
+	}
+	
+	function shouldSuccesValue() external {
+		testContract.setValue("Oke");
+		Assert.equal(testContract.value(), "Oke", "Value shoud have changed to 'Oke' ");
+	}
+	
+	function shouldSeeValue() external {
+		testContract.setValue("Totziens!");
+		Assert.equal(testContract.value(), "Totziens!", "Value shoud have changed to 'Totziens!' ");
+	}
 }
