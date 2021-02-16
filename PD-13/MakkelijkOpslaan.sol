a6020fbf8bd4724bc156f83e8bdced884dd4303f
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-02-16 19:58:03 +0100

    Update MakkelijkOpslaan.sol

--- PD-13/MakkelijkOpslaan.sol
+++ PD-13/MakkelijkOpslaan.sol
@@ -1,33 +1,16 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.1;
 
-/// @title MakkelijkOpslaan save numbers and retrieve them
-
-// import the library from zepplin with this raw githubcontent otherwise you will get a error in remix
-// Relatively simple contract to show the use of the access control
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/access/Ownable.sol";
-
-contract MakkelijkOpslaan is Ownable {
-    string data = "testdata";
-    uint number = 5000;
-   
-    constructor() public Ownable() {
-  
+contract SimpleStorage {
+    
+    event ValueChanged(string oldValue, string newValue);
+    string public value;
+    
+    constructor(string memory _value) public {
+        value = _value;
     }
-    //demonstrate the access control on this function
-    function saveTextData(string memory _data) public onlyOwner {
-        data = _data;
-    }
-    //demonstrate the acces control on this function 
-    function GetTextData() public onlyOwner view returns (string memory) {
-        return data;
-    }
-    //demonstration a function without the acces control
-    function SaveNumberData(uint _number) public{
-        number = _number;
-    }
-    //demonstration a function without the acces control
-    function GetNumberData()public view returns(uint)
-    {
-        return number;
+    
+    function setValue(string memory _value) public {
+        emit ValueChanged(value, _value);
+        value = _value;
     }
 }
