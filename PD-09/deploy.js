7d3478da9fb42411cc82e7efecd68dcbea480779
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-02-18 21:52:02 +0100

    Delete deploy.js

--- PD-09/deploy.js
+++
@@ -1,8 +0,0 @@
-
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-var OpslaanGegevens = artifacts.require("OpslaanGegevens");
-
-module.exports = async function(deployer) {
-    const OpslaanGegevens = await deployProxy(OpslaanGegevens, {deployer});
-    console.log(`Address of OpslaanGegevens: ${OpslaanGegevens.address}`)
-};
