c3771d1f44e36e0e41c7dab540d8d6c5b8905a30
Kajalpriyab <52586382+Kajalpriyab@users.noreply.github.com>
2021-03-01 22:33:40 +0100

    Add files via upload

---
+++ PD-11/kajalweek10erc721/contracts/nft.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract NFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT", "KJAL") public{
+    }
+
+    function CreateNFT(address tokenholder, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(tokenholder, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
